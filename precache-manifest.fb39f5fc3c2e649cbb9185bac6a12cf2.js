54ae5b2164c7c8d87c291f12626ef631c0019001
Marion Lambert <marionlambert85@googlemail.com>
2019-03-04 12:05:30 +0100

    Updates

---
+++ precache-manifest.fb39f5fc3c2e649cbb9185bac6a12cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba3517bfce1346f5e2a",
+    "url": "/memory/static/js/runtime~main.bba3517b.js"
+  },
+  {
+    "revision": "ffdddcbc0d198a5f0b67",
+    "url": "/memory/static/js/main.ffdddcbc.chunk.js"
+  },
+  {
+    "revision": "dae344d2c9c88ad041d8",
+    "url": "/memory/static/js/2.dae344d2.chunk.js"
+  },
+  {
+    "revision": "ffdddcbc0d198a5f0b67",
+    "url": "/memory/static/css/main.ae2ea4ff.chunk.css"
+  },
+  {
+    "revision": "68aeb6150629b1e07fd2d844fc6e2d81",
+    "url": "/memory/index.html"
+  }
+];
